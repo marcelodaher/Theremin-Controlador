dc141cc3062bbd2912dbe091113d2005e51c65fc
marcelo <bidao_@hotmail.com>
2012-09-05 00:33:26 -0300

    / ‘Programação/print_freq_ou_midi.py’

--- Programação/print_freq_ou_midi.py
+++ Programação/print_freq_ou_midi.py
@@ -1,3 +1,5 @@
+#programa pra ler e imprimir a frequencia do microfone dada uma certa taixa de atualizacao
+
 # biblioteca SoundAnalyse pelo pacote 'import analyse'
 import analyse
 
@@ -11,13 +13,11 @@ import time
 
 #os.system('clear')
 
-#colocando uma taxa de atualizacao de 30Hz
-
 tempo_anterior = 0
 
 while 1 == 1:
 	tempo_atual = time.time()
-	if tempo_atual - tempo_anterior > 0.02:
+	if tempo_atual - tempo_anterior > 0.02: #atualizacao 30Hz
 		print "%.2f" % tempo_atual
 		tempo_anterior=tempo_atual
 
